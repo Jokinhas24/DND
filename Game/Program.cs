918694422ff07d968ee68df279e8eff19b39b34b
João Nogueira <joaonogueira.art@gmail.com>
2024-03-09 16:57:52 +0000

    Added most DND game option and exit

---
+++ Game/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            string GameAction;
+            bool InCombat = false;
+
+            //Start the Game
+            Console.WriteLine("Welcome to D&D!");
+
+            //Game Loop
+            while (true)
+            {
+                //Game Action Choosing
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("1. Skill Check");
+                Console.WriteLine("2. Saving Throws");
+                Console.WriteLine("3. Enter Combat");
+                Console.WriteLine("4. Short Rest");
+                Console.WriteLine("4. Long Rest");
+                Console.WriteLine("or type 'EXIT' to Quit.");
+
+                GameAction = Console.ReadLine();
+
+                //Exit
+                if (GameAction == "EXIT")
+                {
+                    break;
+                }
+                //Skill Check
+                else if (GameAction == "1")
+                {
+                    Console.WriteLine("What Skill?");
+                }
+                //Not re
+                else
+                {
+                    Console.WriteLine("Game Action not defined.");
+                }
+            }
+        }
+    }
+}
